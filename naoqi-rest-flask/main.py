5099518460497b40046f683ad98d71c25fa8d836
Kyrylo Yefremov <kyrylo.yefremov@tul.cz>
2024-08-25 18:29:30 +0200

    add home route. add route (test version) for robot init

--- naoqi-rest-flask/main.py
+++ naoqi-rest-flask/main.py
@@ -1,10 +1,43 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import Robot as R
+
 app = Flask(__name__)
+robot = None
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    response = {
+        "status": 200,
+        'info': 'Connection is OK.'
+    }
+    return jsonify(response)
+
+
+@app.route('/init-robot', methods=['POST'])
+def init_robot():
+    if (not request.is_json):
+        response = {
+            "status": 404,
+            "error": "Data must be in JSON format."
+        }
+        return jsonify(response)
+
+    global robot
+    robot_data = request.get_json()
+
+    ip = robot_data.get("ip")
+    port = robot_data.get("port")
+    name = robot_data.get("name")
+
+    print ip, port, name
+    # robot = R.Robot(r_id, r_port, r_name)
+
+    response = {
+        "status": 200,
+        "info": "Connection between IDE and robot was established."
+    }
+    return jsonify(response)
 
 
 if (__name__ == '__main__'):
